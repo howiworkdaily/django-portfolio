2f14583f97f8183ee669207f0d355f932d875b39
Will Larson <lethain@gmail.com>
2008-12-18 21:44:57 -0800

    Initial implementation of admin.py

---
+++ portfolio/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from portfolio.models import *
+
+admin.site.register(Project)
+admin.site.register(Skill)
+admin.site.register(Category)
+admin.site.register(ProjectFile)
+admin.site.register(ProjectImage)
