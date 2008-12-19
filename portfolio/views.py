60d0e25493a8c46be1b80ac097bfac146e180e8c
Will Larson <lethain@gmail.com>
2008-12-18 22:21:04 -0800

    Added project based custom context to all views. (This may be a bad idea. But it seems like a good idea.)

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,47 +1,64 @@
 from django.views.generic.list_detail import object_detail, object_list
 from models import Project, Skill, Category
 
+def project_context():
+    return {
+        'project_list': Project.objects.all(),
+        'category_list': Category.objects.all(),
+        'skill_list': Skill.objects.all(),
+        }
+
 def project_detail(request, slug, template_name='portfolio/project_detail.html', extra_context={}):
+    extra = project_context()
+    extra.update(extra_context)
     return object_detail(
         request,
         template_name = template_name,
-        extra_context = extra_context,
+        extra_context = extra,
         slug = slug,
         slug_field = 'slug',
         queryset = Project.objects.all(),
         )
 
 def project_list(request, template_name='portfolio/project_list.html', extra_context={}):
+    extra = project_context()
+    extra.update(extra_context)
     return object_list(
         request,
         template_name = template_name,
-        extra_context = extra_context,
+        extra_context = extra,
         queryset = Project.objects.all(),
         )
 
 def category_detail(request, slug, template_name='portfolio/category_detail.html', extra_context={}):
+    extra = project_context()
+    extra.update(extra_context)
     return object_detail(
         request,
         template_name = template_name,
-        extra_context = extra_context,
+        extra_context = extra,
         slug = slug,
         slug_field = 'slug',
         queryset = Category.objects.all(),
         )
 
 def category_list(request, template_name='portfolio/category_list.html', extra_context={}):
+    extra = project_context()
+    extra.update(extra_context)
     return object_list(
         request,
         template_name = template_name,
-        extra_context = extra_context,
+        extra_context = extra,
         queryset = Category.objects.all(),
         )
 
 def skill_detail(request, slug, template_name='portfolio/skill_detail.html', extra_context={}):
+    extra = project_context()
+    extra.update(extra_context)
     return object_detail(
         request,
         template_name = template_name,
-        extra_context = extra_context,
+        extra_context = extra,
         slug = slug,
         slug_field = 'slug',
         queryset = Skill.objects.all(),
@@ -49,9 +66,11 @@ def skill_detail(request, slug, template_name='portfolio/skill_detail.html', ext
 
 
 def skill_list(request, template_name='portfolio/skill_list.html', extra_context={}):
+    extra = project_context()
+    extra.update(extra_context)
     return object_list(
         request,
         template_name = template_name,
-        extra_context = extra_context,
+        extra_context = extra,
         queryset = Skill.objects.all(),
         )
