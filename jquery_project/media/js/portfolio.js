b127ac793ae6dcd5bff62beaddc10b5695794ad5
Will Larson <lethain@gmail.com>
2008-12-28 16:42:30 -0500

    Can now click through images on project detail pages.

--- jquery_project/media/js/portfolio.js
+++ jquery_project/media/js/portfolio.js
@@ -1,13 +1,22 @@
 $(document).ready(function() {
     var index = -1;
     var images = $("#images");
-    var len = images.children().length;
+    var len = images.children('li').length;
+    var make_counter = function() {
+      return $("<span id=\"counter\">" + (index+1) + "/" + len + "</span>");
+    }
+
+    
 
     var show_next = function() {
       index = (index+1) % len;
-      $(images.children()).hide();
-      $(images.children()[index]).show();
+      $(images.children('li')).hide();
+      $(images.children('li')[index]).show();
+      $("#counter").replaceWith(make_counter());
     }
+    
+    $("#images").prepend(make_counter());
+    
     show_next();
     $("#images").click(show_next);
   });
