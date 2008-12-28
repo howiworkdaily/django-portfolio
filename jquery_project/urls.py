7d3337a5aa5163b413ca19a6e66c6e901b36bbef
Will Larson <lethain@gmail.com>
2008-12-28 10:49:27 -0500

    Updated gitignore.

---
+++ jquery_project/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.defaults import *
+
+admin.autodiscover()
+
+urlpatterns = patterns(
+    '',
+    (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT }),
+    (r'^admin/(.*)', admin.site.root),
+    (r'^portfolio/', include('portfolio.urls')),    
+)
