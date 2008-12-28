10b6c80cccaebb36b8b84d2e0bc91416d0b19803
Will Larson <lethain@gmail.com>
2008-12-28 13:31:25 -0500

    Now cycles through a project's images on click (in detail view only).

--- jquery_project/media/js/portfolio.js
+++ jquery_project/media/js/portfolio.js
@@ -1,1 +1,13 @@
-alert("hi");
+$(document).ready(function() {
+    var index = -1;
+    var images = $("#images");
+    var len = images.children().length;
+
+    var show_next = function() {
+      index = (index+1) % len;
+      $(images.children()).hide();
+      $(images.children()[index]).show();
+    }
+    show_next();
+    $("#images").click(show_next);
+  });
