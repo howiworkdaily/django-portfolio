f82c37b26514ce036049108fa2b857bd69a30218
Will Larson <lethain@gmail.com>
2008-12-28 21:58:45 -0500

    Fixed JS creating duplicate image counters.

--- jquery_project/media/js/portfolio.js
+++ jquery_project/media/js/portfolio.js
@@ -5,18 +5,13 @@ $(document).ready(function() {
     var make_counter = function() {
       return $("<span id=\"counter\">" + (index+1) + "/" + len + "</span>");
     }
-
-    
-
     var show_next = function() {
       index = (index+1) % len;
       $(images.children('li')).hide();
       $(images.children('li')[index]).show();
-      $("#counter").replaceWith(make_counter());
+      $("#counter").remove();
+      $(images).prepend(make_counter());
     }
-    
-    $("#images").prepend(make_counter());
-    
     show_next();
-    $("#images").click(show_next);
+    $(images).click(show_next);
   });
